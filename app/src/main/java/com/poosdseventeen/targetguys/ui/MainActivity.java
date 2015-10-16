1b3e5f71937a254a8f16fc032a05f78f020beb97
James Bryant <j.alexanderbryant@knights.ucf.edu>
2015-10-16 02:09:41 -0400

    Start Login page; Upgrade some gradle stuff

--- app/src/main/java/com/poosdseventeen/targetguys/ui/MainActivity.java
+++ app/src/main/java/com/poosdseventeen/targetguys/ui/MainActivity.java
@@ -1,9 +1,11 @@
-package com.poosdseventeen.targetguys;
+package com.poosdseventeen.targetguys.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.poosdseventeen.targetguys.R;
 
 public class MainActivity extends AppCompatActivity {
 
