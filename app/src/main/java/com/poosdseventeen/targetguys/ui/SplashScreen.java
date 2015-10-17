1e243e37ffb07de701dfdae0cf13d9c31461f067
Kourtney Bosshardt <kbosshardt@knights.ucf.edu>
2015-10-17 12:43:34 -0400

    Created Splash Screen.

---
+++ app/src/main/java/com/poosdseventeen/targetguys/ui/SplashScreen.java
@@ -0,0 +1,41 @@
+package com.poosdseventeen.targetguys.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.poosdseventeen.targetguys.R;
+
+/**
+ * Created by Kourtney on 10/17/15.
+ */
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
