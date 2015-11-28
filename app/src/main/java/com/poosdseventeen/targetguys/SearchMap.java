f20db990abaf34bfa4fb76797cf588e6ce3ab3fe
themilkman993 <colinkleinkopf@gmail.com>
2015-11-28 13:24:06 -0500

    Location now pulls from the DB if it can't get your current position from a provider, a circle with a radius of 10 miles is shown around your location, the camera now zooms and moves to your position, other users are now displayed on the map, and your current location will be pushed to the DB whenever you use the map

--- app/src/main/java/com/poosdseventeen/targetguys/SearchMap.java
+++ app/src/main/java/com/poosdseventeen/targetguys/SearchMap.java
@@ -1,31 +1,42 @@
 package com.poosdseventeen.targetguys;
 
 import android.app.AlertDialog;
-import android.app.PendingIntent;
-import android.app.TaskStackBuilder;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.parse.FindCallback;
-import com.parse.ParseException;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
-import java.util.ArrayList;
+import java.text.ParseException;
 import java.util.List;
 
 /**
@@ -37,10 +48,6 @@ import java.util.List;
 public class SearchMap extends AppCompatActivity {
     static final LatLng testPoint = new LatLng(21, 57);
     private GoogleMap googleMap;
-    private ParseUser currentUser;
-
-
-    ArrayList showUsers =  new ArrayList<String>();
 
 
     @Override
@@ -48,78 +55,95 @@ public class SearchMap extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_search);
 
-        currentUser = ParseUser.getCurrentUser();
-
-//        try {
-//            if (googleMap == null) {
-//                googleMap = ((MapFragment) getFragmentManager().
-//                        findFragmentById(R.id.map)).getMap();
-//            }
-//            googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-//            // Marker TP = googleMap.addMarker(new MarkerOptions().
-//            //        position(testPoint).title("testPoint"));
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-
-        // Do a null check to confirm that we have not already instantiated the map.
-        if (googleMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
-//            googleMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
-            googleMap = ((MapFragment) getFragmentManager().
+        try {
+            if (googleMap == null) {
+                googleMap = ((MapFragment) getFragmentManager().
                         findFragmentById(R.id.map)).getMap();
-            //googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-            googleMap.setMyLocationEnabled(true);
-            // Check if we were successful in obtaining the map.
-            if (googleMap != null) {
-
-
-                googleMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
-
-                    @Override
-                    public void onMyLocationChange(Location arg0) {
-                        // TODO Auto-generated method stub
-
-                        googleMap.addMarker(new MarkerOptions().position(new LatLng(arg0.getLatitude(), arg0.getLongitude())).title("It's Me!"));
-//                        Log.d("Lattitude", arg0.getLatitude() + "");
-//                        Log.d("Longitude", arg0.getLongitude() + "");
-//                        ParseGeoPoint point = new ParseGeoPoint(arg0.getLatitude(), arg0.getLongitude());
-//                        currentUser.put("location", point);
-                    }
-                });
-
             }
+            googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            // Marker TP = googleMap.addMarker(new MarkerOptions().
+            //        position(testPoint).title("testPoint"));
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-
-        setUpUsersList();
+        setUpMap();
     }
 
-//    private void setUpMap() {
-//
-//        // Enable MyLocation Layer of Google Map
-//        googleMap.setMyLocationEnabled(true);
-//
-//        LatLng yourLocation = getLocation();
-//        //get Your Current Location
-//
-//        if(yourLocation != null) {
-//            Marker TP = googleMap.addMarker(new MarkerOptions().
-//                    position(yourLocation).title("Your location!"));
-//        }
-//        else{
-//            /////USE THE PREVIOUSLY STORED LOCATION FROM THE DATABASE, IF ONE EXISTS
-//        }
-//
-//        ///////////////////////////////////////////////////////////////////////////////////////////
-//        //ADD CODE HERE TO INSERT yourLocation INTO THE DATABASE FOR THE CURRENT USER////////////
-//        //////////////////////////////////////////////////////////////////////////////////////////
-//
-//        //////////////////////////////////////////////////////////////////////////////////////////
-//        ///ADD CODE HERE TO ALSO DISPLAY OTHER USERS' LOCATION NEARBY////////////////////////////
-//        //////////////////////////////////////////////////////////////////////////////////////////
-//
-//    }
+    private void setUpMap() {
+
+        // Enable MyLocation Layer of Google Map
+        googleMap.setMyLocationEnabled(true);
+
+        LatLng yourLocation = getLocation();
+        //get Your Current Location
+
+        ParseUser currentUser = ParseUser.getCurrentUser();
+
+
+        if(currentUser == null) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        if(yourLocation == null) {
+            ParseGeoPoint point = currentUser.getParseGeoPoint("location");
+            if(point == null){
+                AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
+
+                dlgAlert.setMessage("No location can be found, please open up Google Maps to get your position then try again.");
+                dlgAlert.setTitle("Error Message...");
+                dlgAlert.setPositiveButton("OK", null);
+                dlgAlert.setCancelable(true);
+                dlgAlert.create().show();
+
+                finish();
+            }
+            yourLocation = new LatLng(point.getLatitude(), point.getLongitude());
+
+            /////USE THE PREVIOUSLY STORED LOCATION FROM THE DATABASE, IF ONE EXISTS
+        }
+
+        Marker TP = googleMap.addMarker(new MarkerOptions().
+                position(yourLocation).title(currentUser.getString("name")));
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(yourLocation, 10);
+        googleMap.animateCamera(cameraUpdate);
+
+        Circle circle = googleMap.addCircle(new CircleOptions()
+                .center(yourLocation)
+                .radius(16093)
+                .strokeColor(Color.RED));
+        circle.setCenter(yourLocation);
+
+
+        currentUser.put("location", new ParseGeoPoint(yourLocation.latitude, yourLocation.longitude));
+
+
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.whereExists("location");
+        query.whereNotEqualTo("objectId", currentUser.getString("objectId"));
+        query.findInBackground(new FindCallback<ParseUser>() {
+            public void done(List<ParseUser> userList, com.parse.ParseException e) {
+                if (e == null) {
+                    for (int i = 0; i < userList.size(); i++) {
+                        googleMap.addMarker(new MarkerOptions().position(new LatLng(userList.get(i).getParseGeoPoint("location").getLatitude(), userList.get(i).getParseGeoPoint("location").getLongitude())).title(userList.get(i).getString("name")));
+                    }
+
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "Error loading user locations",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+        //////////////////////////////////////////////////////////////////////////////////////////
+        ///ADD CODE HERE TO ALSO DISPLAY OTHER USERS' LOCATION NEARBY////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////////////
+
+    }
 
     public LatLng getLocation() {
         // Get the location manager
@@ -136,84 +160,5 @@ public class SearchMap extends AppCompatActivity {
             e.printStackTrace();
             return null;
         }
-    }
-
-    private void setUpUsersList(){
-
-        //NEED TO CHANGE THIS QUERY!
-        ParseQuery<ParseUser> query = ParseUser.getQuery();
-        query.whereEqualTo("username", "user2");
-        query.findInBackground(new FindCallback<ParseUser>() {
-            public void done(List<ParseUser> objects, ParseException e) {
-
-                ParseUser user;
-                if (e == null) {
-                    for (int i = 0; i < objects.size(); i++) {
-                        user = objects.get(i);
-                        showUsers.add(user.getString("name"));
-                    }
-
-                } else {
-                    // Something went wrong.
-                }
-            }
-        });
-
-        ListView usersList =(ListView)findViewById(R.id.users_listview);
-        ArrayAdapter<String> arrayAdapter =
-                new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, showUsers);
-        // Set The Adapter
-        usersList.setAdapter(arrayAdapter);
-
-        // register onClickListener to handle click events on each item
-        usersList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            // argument position gives the index of item which is clicked
-            public void onItemClick(AdapterView<?> arg0, View v, int position, long arg3) {
-
-                final String selectedUser = showUsers.get(position).toString();
-
-                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(SearchMap.this);
-                alertDialogBuilder.setMessage("Choose an option");
-
-                // get an existing photo form the gallery
-                alertDialogBuilder.setPositiveButton("View Profile", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface arg0, int arg1) {
-                        Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
-                        intent.putExtra("selectedUser", selectedUser);
-
-                        PendingIntent pendingIntent =
-                                TaskStackBuilder.create(SearchMap.this)
-                                        .addNextIntentWithParentStack(intent).getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT);
-                        NotificationCompat.Builder builder = new NotificationCompat.Builder(SearchMap.this);
-                        builder.setContentIntent(pendingIntent);
-
-                        startActivity(intent);
-                    }
-                });
-
-                // get new photo from camera
-                alertDialogBuilder.setNegativeButton("Send Message", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                        Intent intent = new Intent(getApplicationContext(), ChatActivity.class);
-
-                        PendingIntent pendingIntent =
-                                TaskStackBuilder.create(SearchMap.this)
-                                        .addNextIntentWithParentStack(intent).getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT);
-                        NotificationCompat.Builder builder = new NotificationCompat.Builder(SearchMap.this);
-                        builder.setContentIntent(pendingIntent);
-
-                        startActivity(intent);
-
-
-                    }
-                });
-
-                AlertDialog alertDialog = alertDialogBuilder.create();
-                alertDialog.show();
-            }
-        });
     }
 }
